7d66257c5ed02d37011ade055f89cee887e3c0cc
USER <USER@USER-PC>
2019-04-16 12:07:13 +0900

    new/dev : HelloApp 생성

---
+++ z.StudyGit/src/test/HelloApp.java
@@ -0,0 +1,18 @@
+package test;
+
+public class HelloApp {
+
+	public HelloApp() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void newMethod() {
+		System.out.println("new/dev 에서 newMethod 추가요.");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
