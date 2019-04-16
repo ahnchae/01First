698b9b7069845bf106e41d5921dd06b9a0b78026
USER <USER@USER-PC>
2019-04-16 12:16:38 +0900

    test/dev : HelloApp 내부 수정작업함.

--- z.StudyGit/src/test/HelloApp.java
+++ z.StudyGit/src/test/HelloApp.java
@@ -8,6 +8,8 @@ public class HelloApp {
 	
 	public void newMethod() {
 		System.out.println("new/dev 에서 newMethod 추가요.");
+
+		System.out.println("test/dev 에서 newMethod 내부수정작업함.");	
 	}
 
 	public static void main(String[] args) {
