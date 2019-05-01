a3c6070ea4e08fe999ba80696bc6ab5de2448b00
USER <USER@USER-PC>
2019-05-01 09:38:50 +0900

    new/dev : return type String

---
+++ z.StudytGit/src/test/HelloApp.java
@@ -0,0 +1,25 @@
+package test;
+
+public class HelloApp {
+
+	public HelloApp() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void newMethod() {
+
+		System.out.println("new method 추가요");
+		
+		
+		System.out.println("test/dev에서 new method 내부수정작업함");
+		System.out.println("추가요");
+		
+		System.out.println("추가추가요!!!");
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
